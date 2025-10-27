9095e058c17266edec46a197161e861b3cfc725c
faiwer <faiwer@gmail.com>
2025-10-27 21:04:30 +0100

    jest & tsconfig update

---
+++ jest.config.js
@@ -0,0 +1,34 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(t|j)sx?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          parser: { syntax: 'typescript', tsx: true },
+          transform: {
+            react: {
+              // no need in "import React"
+              runtime: 'automatic',
+              // import { jsx } from '<...this...>';
+              importSource: 'faiwer-react',
+              development: true,
+            },
+          },
+          target: 'es2022',
+        },
+        module: { type: 'commonjs' },
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '^faiwer-react/jsx-runtime$': '<rootDir>/src/jsx-runtime.ts',
+    '^faiwer-react/jsx-dev-runtime$': '<rootDir>/src/jsx-dev-runtime.ts',
+    '^faiwer-react$': '<rootDir>/src/index.ts',
+    '^faiwer-react/(.*)$': '<rootDir>/src/$1',
+    '^~/(.*)$': '<rootDir>/src/$1',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  roots: ['<rootDir>/tests', '<rootDir>/src'],
+};
