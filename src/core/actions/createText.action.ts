8062f449da355b9b39ff92702ffcd32097e97a4b
faiwer <faiwer@gmail.com>
2025-10-31 23:27:18 +0100

    actions: createText

--- src/core/actions/createText.action.ts
+++ src/core/actions/createText.action.ts
@@ -1,5 +1,15 @@
 import type { FiberNode } from 'faiwer-react/types';
+import { getParentElement } from './helpers';
 
 export function createTextAction(fiber: FiberNode) {
-  // TODO
+  if (fiber.type !== 'text') {
+    throw new Error(`Can't apply CreateText for a ${fiber.type} node`);
+  }
+
+  // Only two scenarios lead here:
+  // - 1st render of a fragment or a component. The parent's element is
+  //   a comment `r:begin:{id}`
+  // - Render of a portal. The parent element is the portal target node.
+  fiber.element = new Text(fiber.props.text);
+  getParentElement(fiber).appendChild(fiber.element);
 }
