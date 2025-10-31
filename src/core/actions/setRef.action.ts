0d2411df503ee18863cb85771f3d593ebdd7d473
faiwer <faiwer@gmail.com>
2025-11-01 00:29:06 +0100

    actions: setRef, draft

--- src/core/actions/setRef.action.ts
+++ src/core/actions/setRef.action.ts
@@ -1,9 +1,46 @@
 import type { FiberNode } from 'faiwer-react/types';
+import { getAppByFiber, invalidateEffect } from '../reconcile';
+import { nullthrows } from 'faiwer-react/utils';
 import type { SetRefAction } from 'faiwer-react/types/actions';
+import { unsetRef } from './helpers';
 
+/**
+ * - A ref of a ref handler for the given node was:
+ *   - set up
+ *   - or replaced
+ */
 export function setRefAction(
   fiber: FiberNode,
   { ref, dontUnsetRef }: SetRefAction,
 ) {
-  // TODO
+  if (fiber.type !== 'tag') {
+    throw new Error(`setRefAction is not compatible with ${fiber.type} nodes`);
+  }
+
+  if (
+    fiber.ref &&
+    // It's `true` on the 1st render of the tag. We shouldn't run `onRef(null)`
+    // in such a case
+    !dontUnsetRef
+  ) {
+    // ref.current = null | ref(null)
+    unsetRef(fiber.ref);
+  }
+
+  // Run effects only when the DOMNode is already mounted to a real DOMnode.
+  invalidateEffect(
+    getAppByFiber(fiber),
+    () => {
+      const element = nullthrows(fiber.element) as HTMLElement;
+      if (typeof ref === 'function') {
+        ref(element);
+      } else if (ref) {
+        ref.current = element;
+      }
+    },
+    'layout',
+  );
+
+  fiber.ref = ref as typeof fiber.ref;
+  return;
 }
