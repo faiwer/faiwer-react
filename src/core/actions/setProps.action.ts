1f9c007ecbe8e9e95897e49e3205a39b7b9a02f2
faiwer <faiwer@gmail.com>
2025-10-31 23:33:10 +0100

    actions: setPropsAction

--- src/core/actions/setProps.action.ts
+++ src/core/actions/setProps.action.ts
@@ -1,6 +1,16 @@
 import type { FiberNode } from 'faiwer-react/types';
 import type { SetPropsAction } from 'faiwer-react/types/actions';
 
+/**
+ * Updates the set of props for:
+ * - a component fiber
+ * - or a context fragment fiber
+ */
 export function setPropsAction(fiber: FiberNode, { props }: SetPropsAction) {
-  // TODO
+  if (fiber.type !== 'component' && fiber.role !== 'context') {
+    throw new Error(
+      `setProps action is only applicable to component and context fiber nodes`,
+    );
+  }
+  fiber.props = props;
 }
