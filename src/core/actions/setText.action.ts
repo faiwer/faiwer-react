9b52c6989074fa872220402b3ebd1e53ffb8b9a3
faiwer <faiwer@gmail.com>
2025-10-31 23:29:35 +0100

    actions: setTextAction

--- src/core/actions/setText.action.ts
+++ src/core/actions/setText.action.ts
@@ -2,6 +2,13 @@ import type { FiberNode } from 'faiwer-react/types';
 import type { SetTextAction } from 'faiwer-react/types/actions';
 import { nullthrows } from 'faiwer-react/utils';
 
+/**
+ * Replaces the text content within a Text DOM-Node.
+ */
 export function setTextAction(fiber: FiberNode, { text }: SetTextAction) {
-  // TODO
+  if (fiber.type !== 'text') {
+    throw new Error(`Can't apply setText action to non-text node`);
+  }
+  fiber.props = { text };
+  nullthrows(fiber.element).textContent = text;
 }
