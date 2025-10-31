afd520936815fbe1502a7f4ca86734f6e3a0b4cd
faiwer <faiwer@gmail.com>
2025-10-31 22:57:24 +0100

    actions: createCommentAction

--- src/core/actions/createComment.action.ts
+++ src/core/actions/createComment.action.ts
@@ -1,9 +1,23 @@
 import type { FiberNode } from 'faiwer-react/types';
 import type { CreateCommentAction } from 'faiwer-react/types/actions';
+import { buildComment } from '../helpers';
+import { getParentElement } from './helpers';
+import { tryToCompactNode } from '../compact';
 
 export function createCommentAction(
   fiber: FiberNode,
   { mode }: CreateCommentAction,
 ) {
-  // TODO
+  // Only two scenarios lead here:
+  // - 1st render of a fragment or a component. The parent's element is a
+  //   comment `r:begin:{id}`
+  // - Render of a portal. The parent element is the portal target node.
+  fiber.element = buildComment(mode, fiber.id);
+  getParentElement(fiber).appendChild(fiber.element);
+
+  if (mode === 'end') {
+    // All children are added. If the current fiber is the only child we can
+    // compact the parent node. "<!--begin-->child<!--end-->" -> "child".
+    tryToCompactNode(fiber);
+  }
 }
