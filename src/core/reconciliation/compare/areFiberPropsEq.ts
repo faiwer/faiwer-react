2c61f2b31cd64484afe4f3ed9e73481e7c35c904
faiwer <faiwer@gmail.com>
2025-11-02 22:57:53 +0100

    reconciliation: areFiberPropsEq

---
+++ src/core/reconciliation/compare/areFiberPropsEq.ts
@@ -0,0 +1,26 @@
+import type { FiberNode, UnknownProps } from 'faiwer-react/types';
+
+/**
+ * Returns false when `r` !== `l` and requires to be updated. It doesn't involve
+ * any `children` checks.
+ */
+export const areFiberPropsEq = (l: FiberNode, r: FiberNode): boolean => {
+  if (l.ref !== r.ref) {
+    return false;
+  }
+
+  if (l.role !== r.role) {
+    return false;
+  }
+
+  const lProps = (l.props ?? EMPTY) as UnknownProps;
+  const rProps = (r.props ?? EMPTY) as UnknownProps;
+  const lPropKeys = Object.keys(lProps);
+  if (lPropKeys.length !== Object.keys(rProps).length) {
+    return false;
+  }
+
+  return lPropKeys.every((k) => lProps[k] === rProps[k]);
+};
+
+const EMPTY = {};
