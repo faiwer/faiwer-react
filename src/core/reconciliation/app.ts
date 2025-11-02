26877948d7abddc1ccbb50c74a10ac86ddd95099
faiwer <faiwer@gmail.com>
2025-11-02 16:11:38 +0100

    reconciliation: app

---
+++ src/core/reconciliation/app.ts
@@ -0,0 +1,24 @@
+import type { App, FiberNode } from 'faiwer-react/types';
+import { nullthrows } from 'faiwer-react/utils';
+
+const apps: Array<App | null> = [];
+
+export const getAppByFiber = (fiber: FiberNode): App => {
+  return nullthrows(apps[fiber.appId]);
+};
+
+export const registerApp = (fn: (id: number) => App): App => {
+  apps.push(fn(apps.length));
+  return apps.at(-1)!;
+};
+
+export const removeApp = (id: number): void => {
+  const app = apps[id] as Record<string, unknown>;
+  if (app) {
+    apps[id] = null;
+    // Help GC.
+    app.root = app.tempContext = null;
+    app.effects = { normal: [] };
+    app.invalidatedComponents = { size: 0 };
+  }
+};
