166c77cddd575b16b06ec496aa9f8ef98f82868d
faiwer <faiwer@gmail.com>
2025-11-02 18:52:52 +0100

    reconciliation: applyActions

---
+++ src/core/reconciliation/applyActions.ts
@@ -0,0 +1,14 @@
+import type { App } from 'faiwer-react/types';
+import { applyAction } from '../actions/applyAction';
+import type { Action } from 'faiwer-react/types/actions';
+
+/**
+ * Moves the app into the "commit" state and applies the given actions one by
+ * one.
+ */
+export const applyActions = (app: App, actions: Action[]): void => {
+  app.state = 'commit';
+  for (const action of actions) {
+    applyAction(action);
+  }
+};
