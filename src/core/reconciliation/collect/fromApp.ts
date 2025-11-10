7c943b3a109bc274b39517c00d87471aaaa63871
faiwer <faiwer@gmail.com>
2025-11-10 23:59:00 +0100

    nits

--- src/core/reconciliation/collect/fromApp.ts
+++ src/core/reconciliation/collect/fromApp.ts
@@ -1,7 +1,6 @@
 import type { App } from 'faiwer-react/types';
 import type { Action } from 'faiwer-react/types/actions';
 import { collectActionsFromComponent } from './fromComponent';
-import { validateApp } from '../validateApp';
 
 /**
  * Goes through the list of invalidated components, runs them, finds the diff,
@@ -15,8 +14,6 @@ export const collectActionsFromApp = (app: App): Action[] => {
     const [fiber, props] = app.invalidatedComponents.poll();
     actions.push(...collectActionsFromComponent(fiber, props));
   }
-
-  if (app.testMode) validateApp(app);
 
   return actions;
 };
