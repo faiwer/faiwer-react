8e63c660db6d749fbddd27e98d454fe52f97f8a2
faiwer <faiwer@gmail.com>
2025-11-13 10:01:33 +0100

    tests: onChange, more tests

--- tests/core/onChange.test.tsx
+++ tests/core/onChange.test.tsx
@@ -1,5 +1,5 @@
 import { nullthrows } from 'faiwer-react/utils';
-import { expectHtml, mount, useStateX, waitFor } from '../helpers';
+import { expectHtml, mount, useStateX, wait, waitFor } from '../helpers';
 import { act } from 'faiwer-react/testing';
 
 describe('onChange', () => {
@@ -18,6 +18,7 @@ describe('onChange', () => {
   it('It keeps input.value static when the "value" prop is given', async () => {
     const onChange = jest.fn();
     const v = useStateX<string>();
+    const addEventListener = jest.spyOn(Element.prototype, 'addEventListener');
 
     const Comp = () => (
       <input
@@ -28,9 +29,14 @@ describe('onChange', () => {
 
     const root = mount(<Comp />);
     const input = root.querySelector('input')!;
+
     expectHtml(root).toBe('<input>');
     expect(get.call(input)).toBe('42');
     expect(onChange).toHaveBeenCalledTimes(0);
+    expect(addEventListener.mock.calls).toEqual([
+      ['input', expect.any(Function), { capture: true }],
+      ['input', expect.any(Function), { capture: false }],
+    ]);
 
     changeByUser(input, '21');
     await waitFor(() => {
@@ -43,10 +49,95 @@ describe('onChange', () => {
     await act(() => v.set('22'));
     expect(get.call(input)).toBe('22');
     expect(onChange).toHaveBeenCalledTimes(1);
+    expect(addEventListener).toHaveBeenCalledTimes(2); // still 2.
   });
 
-  it.todo('unsubscribes on deletion');
-  it.todo(`doesn't interfere when the value-prop is nil`);
+  it('unsubscribes on tag removal', async () => {
+    const addEventListener = jest.spyOn(Element.prototype, 'addEventListener');
+    const removeEventListener = jest.spyOn(
+      Element.prototype,
+      'removeEventListener',
+    );
+    const onChange = jest.fn();
+
+    const show = useStateX<boolean>();
+    const Comp = () =>
+      show.use(true) && <input value="42" onChange={onChange} />;
+
+    mount(<Comp />);
+    expect(addEventListener.mock.calls).toEqual([
+      ['input', expect.any(Function), { capture: true }],
+      ['input', expect.any(Function), { capture: false }],
+    ]);
+    expect(removeEventListener).toHaveBeenCalledTimes(0);
+
+    await act(() => show.set(false));
+
+    expect(removeEventListener).toHaveBeenCalledTimes(2);
+    expect(removeEventListener.mock.calls).toEqual(addEventListener.mock.calls);
+  });
+
+  it(`doesn't interfere when the value-prop is nil`, () => {
+    const addEventListener = jest.spyOn(Element.prototype, 'addEventListener');
+    const onChange = jest.fn();
+
+    const root = mount(<input onChange={onChange} />);
+    const input = root.querySelector('input')!;
+
+    expect(addEventListener.mock.calls).toEqual([
+      ['input', expect.any(Function), { capture: false }],
+    ]);
+
+    changeByUser(input, '21');
+    expect(get.call(input)).toBe('21');
+    expect(onChange.mock.calls).toEqual([
+      [
+        expect.objectContaining({
+          target: expect.objectContaining({
+            value: '21',
+          }),
+        }),
+      ],
+    ]);
+  });
+
+  for (const nilValue of [null, undefined]) {
+    it(`doesn't interfere when the value-prop is nil-ed afterwards. ${nilValue}`, async () => {
+      const addEventListener = jest.spyOn(
+        Element.prototype,
+        'addEventListener',
+      );
+      const onChange = jest.fn();
+      const nil = useStateX<boolean>();
+
+      const Comp = () => (
+        <input
+          value={!nil.use(false) ? '42' : (nilValue as undefined)}
+          onChange={onChange}
+        />
+      );
+      const root = mount(<Comp />);
+      const input = root.querySelector('input')!;
+
+      expect(addEventListener.mock.calls).toEqual([
+        ['input', expect.any(Function), { capture: true }],
+        ['input', expect.any(Function), { capture: false }],
+      ]);
+
+      changeByUser(input, '21');
+      await waitFor(() => {
+        expect(get.call(input)).toBe('42');
+      });
+
+      await act(() => nil.set(true));
+
+      changeByUser(input, '12');
+      await wait(5);
+      expect(get.call(input)).toBe('12');
+    });
+  }
+
   it.todo(`doesn't recover the value when setState was called`);
   it.todo(`treats manual .value changes as user events`);
+  it.todo(`the order of value & onChange props doesn't matter`);
 });
